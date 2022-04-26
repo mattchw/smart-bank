59e1f9dc407a86d44cf8d039af5780c7d9ca14da
Matthew Wong <matthew.chohin@gmail.com>
2022-04-27 01:02:00 +0800

    feat: setup test case

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://postgres:test1234@localhost:5432/postgres?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Fail to connect to database:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
