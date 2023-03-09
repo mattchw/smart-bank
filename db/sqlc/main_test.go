077ae27276245af9ed5dffa79d012b8d8ca1e69d
Matthew Wong <matthew.chohin@gmail.com>
2023-03-09 16:08:11 +0000

    test(main): add load config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,18 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgres://postgres:test1234@localhost:5432/smart_bank?sslmode=disable"
+	"github.com/mattchw/smart-bank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Fail to load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Fail to connect to database:", err)
 	}
